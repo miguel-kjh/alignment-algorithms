5942d7f82e5acb8ada295e10c415fb07d711519c
miguel-kjh <miguelangel.kjh@gmail.com>
2024-07-05 15:05:00 +0100

    update bash script

--- train.sh
+++ train.sh
@@ -4,18 +4,18 @@
 models=("EleutherAI/pythia-14m" "EleutherAI/pythia-70m-deduped")
 
 # Parámetros comunes
-epochs=1
+epochs=5
 noise_alpha=5
-tiny_dataset=true
+wandb=true
 
 # Iterar sobre cada modelo en la lista
 for model_name in "${models[@]}"; do
   # Construir la lista de combinaciones de parámetros
   params=(
-    "--model_name $model_name --epochs $epochs --tiny_dataset $tiny_dataset"
-    "--model_name $model_name --epochs $epochs --tiny_dataset $tiny_dataset --neftune_noise_alpha $noise_alpha"
-    "--model_name $model_name --epochs $epochs --tiny_dataset $tiny_dataset --instruction_modelling true"
-    "--model_name $model_name --epochs $epochs --tiny_dataset $tiny_dataset --neftune_noise_alpha $noise_alpha --instruction_modelling true"
+    "--model_name $model_name --epochs $epochs --wandb $wandb"
+    "--model_name $model_name --epochs $epochs --wandb $wandb --neftune_noise_alpha $noise_alpha"
+    "--model_name $model_name --epochs $epochs --wandb $wandb --instruction_modelling true"
+    "--model_name $model_name --epochs $epochs --wandb $wandb --neftune_noise_alpha $noise_alpha --instruction_modelling true"
   )
 
   # Construir el comando final ejecutando python train.py con todas las combinaciones de parámetros

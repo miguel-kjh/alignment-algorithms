7b3f7f4f1eda56cb7f35c9945f07f116be0b22d7
miguel-kjh <miguelangel.kjh@gmail.com>
2024-07-05 17:26:56 +0100

    update

--- train.sh
+++ train.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
 
-# Lista de modelos
-models=("EleutherAI/pythia-14m" "EleutherAI/pythia-70m-deduped")
+models=("EleutherAI/pythia-410m-deduped" "EleutherAI/pythia-1b-deduped" "microsoft/phi-2")
 
-# Parámetros comunes
 epochs=5
 noise_alpha=5
 wandb=true
+default_batch_size=8
+default_block_size=512
 
-# Iterar sobre cada modelo en la lista
 for model_name in "${models[@]}"; do
-  # Construir la lista de combinaciones de parámetros
+  if [[ "$model_name" == "EleutherAI/pythia-1b-deduped" ]]; then
+    batch_size=4
+    block_size=256
+    extra_params="--qlora true"
+  elif [[ "$model_name" == "microsoft/phi-2" ]]; then
+    batch_size=4
+    block_size=256
+    extra_params="--qlora true --lora_target_modules 'fc1,fc2'"
+  else
+    batch_size=$default_batch_size
+    block_size=$default_block_size
+    extra_params=""
+  fi
+
   params=(
-    "--model_name $model_name --epochs $epochs --wandb $wandb"
-    "--model_name $model_name --epochs $epochs --wandb $wandb --neftune_noise_alpha $noise_alpha"
-    "--model_name $model_name --epochs $epochs --wandb $wandb --instruction_modelling true"
-    "--model_name $model_name --epochs $epochs --wandb $wandb --neftune_noise_alpha $noise_alpha --instruction_modelling true"
+    "--model_name $model_name --epochs $epochs --wandb $wandb --batch_size $batch_size --block_size $block_size $extra_params"
+    "--model_name $model_name --epochs $epochs --wandb $wandb --neftune_noise_alpha $noise_alpha --batch_size $batch_size --block_size $block_size $extra_params"
+    "--model_name $model_name --epochs $epochs --wandb $wandb --instruction_modelling true --batch_size $batch_size --block_size $block_size $extra_params"
+    "--model_name $model_name --epochs $epochs --wandb $wandb --neftune_noise_alpha $noise_alpha --instruction_modelling true --batch_size $batch_size --block_size $block_size $extra_params"
   )
 
-  # Construir el comando final ejecutando python train.py con todas las combinaciones de parámetros
+  echo "Model: $model_name"
+
+  # Ejecutar el comando python train.py con todas las combinaciones de parámetros
   for param in "${params[@]}"; do
-    python train.py $param
+    echo train.py $param
   done
 done
+

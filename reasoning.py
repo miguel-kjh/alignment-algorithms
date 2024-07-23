8aaebbe55e14692c8ea6f59183154ca9374a0ee1
miguel-kjh <miguelangel.kjh@gmail.com>
2024-07-23 12:59:57 +0100

    update

---
+++ reasoning.py
@@ -0,0 +1,56 @@
+from datasets import load_dataset
+from token_id import OPEN_IA_API_KEY
+from langchain_openai import ChatOpenAI
+import os
+import pandas as pd
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+import tqdm
+
+
+os.environ["OPENAI_API_KEY"] = OPEN_IA_API_KEY
+MODEL = "gpt-3.5-turbo-0125"
+NUMBER_OF_SAMPLES = 10
+
+model = ChatOpenAI(model=MODEL)
+test_dataset = load_dataset("commonsense_qa", split="train")
+test_dataset = test_dataset.select(range(NUMBER_OF_SAMPLES))
+
+
+template_prompt = """
+Given a multiple-choice question along with the options and the correct answer, provide me with the reasoning behind the correct answer.
+Format:
+Question: {question}
+Options: {options}
+Correct Answer: {correct_answer}
+Always end with: The correct answer is ("letter")
+"""
+
+prompt = ChatPromptTemplate.from_template(template_prompt)
+chain = prompt | model | StrOutputParser()
+
+data = []
+for example in tqdm.tqdm(test_dataset, desc="Generating reasoning"):
+    options = ""
+    for label, text in zip(example["choices"]["label"], example["choices"]["text"]):
+        options += f"({label.lower()}) {text} "
+    input_data = {
+        "question": example["question"],
+        "options": options.strip(),
+        "correct_answer": example["answerKey"].lower()
+    }
+    reasoning = chain.invoke(input_data).split("The correct answer is")[-1].strip()
+    
+    # Agregar los datos a la lista
+    data.append({
+        "question": example["question"],
+        "options": options.strip(),
+        "correct_answer": example["answerKey"].lower(),
+        "reasoning": reasoning
+    })
+
+df = pd.DataFrame(data)
+df.to_excel("questions_with_reasoning.xlsx", index=False)
+
+print("Excel file has been created successfully.")
+
